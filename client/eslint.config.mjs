f0a83c94942c085dda89f8f148db141fa9b153e6
jackjohn7 <branchjohnchristopher@gmail.com>
2024-12-20 14:50:23 -0600

    setup eslint and prettier

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,16 +1,48 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+    {
+        rules: {
+            camelcase: [
+                'error',
+                {
+                    ignoreImports: true,
+                },
+            ],
+            eqeqeq: 'error',
+            'no-useless-assignment': 'warn',
+            'consistent-return': 'error',
+            'dot-notation': 'error',
+            'no-unneeded-ternary': 'warn',
+            'object-shorthand': 'error',
+            'prefer-const': 'warn',
+            'prefer-destructuring': 'error',
+            'no-useless-rename': 'error',
+            'no-cond-assign': 'error',
+            '@typescript-eslint/no-unused-vars': [
+                'error', // Change to 'error' if you want it to be an error
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
