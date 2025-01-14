fac1303fffc66b4eb70dee948e73b589079fbade
Erik-Claros <erikclaros.bus@gmail.com>
2025-01-13 20:11:00 -0600

    Added question and buttons

--- client/app/competitor/page.tsx
+++ client/app/competitor/page.tsx
@@ -4,6 +4,7 @@ import {
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Button } from "@/components/ui/button"
 
 export default function ResizableDemo() {
 
@@ -16,6 +17,43 @@ export default function ResizableDemo() {
                         Question Title
                     </b>
                 </h1>
+                <h1 className='question-title-body'>
+                    <b>
+                        Two Sum
+                    </b>
+                </h1>
+                <div className='question-content'>
+                    <p>
+                        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to the target.
+                        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+                        You can return the answer in any order.
+                    </p>
+
+                    <p>
+                        <b>
+                            Example 1:
+                        </b>
+                        <p>
+                            Input:
+
+                            nums = [2, 7, 11, 15], target = 9
+
+                            Output:
+
+                            [0, 1]
+                        </p>
+
+                    </p>
+
+                    <p>
+                        <b>
+                            Explanation:
+                        </b>
+                        <p>
+                            nums[0] + nums[1] = 2 + 7 = 9, so we return [0, 1].
+                        </p>
+                    </p>
+                </div>
             </div>
         )
     }
@@ -23,13 +61,25 @@ export default function ResizableDemo() {
     // TODO: need to be able to grab info from the text editor and have this func take it as a param
     function runTest() {
         return (
-            <div className='test-button'>
+            <div className='button-container'>
+                <Button className="w-32 h-12 bg-[hsl(224.3,76.3%,48%)] text-white hover:bg-opacity-90">
+                    <b>Test</b>
+                </Button>
+                <Button className="w-32 h-12 bg-[hsl(142.4,71.8%,29.2%)] text-white hover:bg-opacity-90">
+                    <b>Submit</b>
+                </Button>
 
             </div>
         )
     }
 
-
+    function testResults() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
 
     return (
         <div className='panel-container'>
@@ -53,7 +103,7 @@ export default function ResizableDemo() {
                         <ResizableHandle />
                         <ResizablePanel defaultSize={100} className="side-panel">
                             <div>
-                                <span>Test Suite</span>
+                                {runTest()}
                             </div>
                         </ResizablePanel>
                     </ResizablePanelGroup>
