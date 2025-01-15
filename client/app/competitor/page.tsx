b32ef0af3be63a8e5c9b10fccec9b90ff9d58699
Erik-Claros <erikclaros.bus@gmail.com>
2025-01-15 02:17:21 -0600

    Added Libs, added text box, nav menu, and adjusted css

--- client/app/competitor/page.tsx
+++ client/app/competitor/page.tsx
@@ -5,110 +5,119 @@ import {
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import { Button } from "@/components/ui/button"
+import { PropsWithChildren } from 'react';
+import Nav from '@/components/Nav';
+import { Textarea } from '@/components/ui/textarea';
 
-export default function ResizableDemo() {
 
-    // TODO: need to bring in Question Information from host component as am input for this func
-    function getCurrentQuestion() {
-        return (
-            <div className='question-title'>
-                <h1>
-                    <b>
-                        Question Title
-                    </b>
-                </h1>
-                <h1 className='question-title-body'>
-                    <b>
-                        Two Sum
-                    </b>
-                </h1>
-                <div className='question-content'>
-                    <p>
-                        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to the target.
-                        You may assume that each input would have exactly one solution, and you may not use the same element twice.
-                        You can return the answer in any order.
-                    </p>
+var showTestResults: boolean = false;
 
-                    <p>
-                        <b>
-                            Example 1:
-                        </b>
-                        <p>
-                            Input:
+function Code({ children }: PropsWithChildren) {
+    return <code className="bg-slate-800 font-mono py-1 px-2 m-1">{children}</code>;
+}
 
-                            nums = [2, 7, 11, 15], target = 9
+// TODO: need to bring in Question Information from host component as am input for this func
+function getCurrentQuestion() {
+    return (
+        <div className='question-title'>
+            <h1>
+                <b>
+                    Question Title
+                </b>
+            </h1>
+            <h1>Sort</h1>
+            <div>
+                <p>
+                    Given an array of integers, sort the array and return it
+                </p>
 
-                            Output:
+                <div className="flex flex-col gap-2">
+                    <div>
+                        <strong>Input</strong>
+                        <pre className="bg-slate-800 font-mono py-2 px-4">
+                            2 11 15 0
+                        </pre>
+                    </div>
+                    <div>
+                        <strong>Output</strong>
+                        <pre className="bg-slate-800 font-mono py-2 px-4">
+                            0 2 11 15
+                        </pre>
+                    </div>
+                    <div>
+                        <strong>Explanation</strong>
+                        <div>
 
-                            [0, 1]
-                        </p>
-
-                    </p>
-
-                    <p>
-                        <b>
-                            Explanation:
-                        </b>
-                        <p>
-                            nums[0] + nums[1] = 2 + 7 = 9, so we return [0, 1].
-                        </p>
-                    </p>
+                            The expected output is
+                            <Code>0 2 11 15</Code>
+                            because
+                            <Code>0 &lt; 2 &lt; 11 &lt; 15</Code>
+                        </div>
+                    </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
 
-    // TODO: need to be able to grab info from the text editor and have this func take it as a param
-    function runTest() {
-        return (
-            <div className='button-container'>
-                <Button className="w-32 h-12 bg-[hsl(224.3,76.3%,48%)] text-white hover:bg-opacity-90">
-                    <b>Test</b>
-                </Button>
-                <Button className="w-32 h-12 bg-[hsl(142.4,71.8%,29.2%)] text-white hover:bg-opacity-90">
-                    <b>Submit</b>
-                </Button>
-
-            </div>
-        )
-    }
-
-    function testResults() {
-        return(
-            <div>
-                
-            </div>
-        )
-    }
-
+// TODO: need to be able to grab info from the text editor and have this func take it as a param
+function runTest() {
     return (
-        <div className='panel-container'>
-            <ResizablePanelGroup
-                direction="horizontal"
-            >
-                <ResizablePanel defaultSize={15}>
-                    <div className="side-panel">
-                        {getCurrentQuestion()}
-                    </div>
-                </ResizablePanel>
-                <ResizableHandle />
-                <ResizablePanel >
-                    <ResizablePanelGroup direction="vertical">
+        <div className="w-full flex flex-col items-center gap-2 mx-4">
+            <Button className="max-w-72 h-12 bg-blue-700 text-white hover:bg-opacity-90 w-full">
+                <b>Test</b>
+            </Button>
+            <Button className="max-w-72 h-12 bg-green-500 text-white hover:bg-opacity-90 w-full">
+                <b>Submit</b>
+            </Button>
+        </div>
+    )
+}
 
-                        <ResizablePanel defaultSize={400} className="side-panel">
-                            <div>
-                                <span>Editor</span>
+function testResults() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default function Competitor() {
+    return (
+        <div>
+            <Nav/>
+
+            <div className='panel-container'>
+                <ResizablePanelGroup
+                    direction="horizontal"
+                >
+                    <ResizablePanel defaultSize={15}>
+                        <ResizablePanelGroup direction="vertical">
+                            <div className="flex flex-col justify-between h-full py-8">
+                                <div className="side-panel">
+                                    {getCurrentQuestion()}
+                                </div>
+                                <div className="w-full flex flex-row justify-center">
+                                    {runTest()}
+                                </div>
                             </div>
-                        </ResizablePanel>
-                        <ResizableHandle />
-                        <ResizablePanel defaultSize={100} className="side-panel">
-                            <div>
-                                {runTest()}
-                            </div>
-                        </ResizablePanel>
-                    </ResizablePanelGroup>
-                </ResizablePanel>
-            </ResizablePanelGroup>
+                        </ResizablePanelGroup>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle />
+                    <ResizablePanel >
+                        <ResizablePanelGroup direction="vertical">
+                            <ResizablePanel defaultSize={400} >
+                                <div className='flex h-full overflow-hidden'>
+                                    <Textarea />
+                                </div>
+                            </ResizablePanel>
+                            <ResizableHandle withHandle />
+                            <ResizablePanel defaultSize={100}>
+                            </ResizablePanel>
+                        </ResizablePanelGroup>
+                    </ResizablePanel>
+                </ResizablePanelGroup>
+            </div>
         </div>
     );
 }
---
+++ client/components/Nav.tsx
@@ -0,0 +1,107 @@
+
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSeparator,
+    MenubarTrigger,
+} from "@/components/ui/menubar"
+import {
+    Tabs,
+    TabsList,
+    TabsTrigger
+} from "@/components/ui/tabs"
+
+export default function Nav() {
+    return (
+        <div className="min-w-full flex justify-between item-center p-2">
+            <Menubar>
+                <MenubarMenu>
+                    <MenubarTrigger>File</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            Import File
+                        </MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Edit</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>
+                            Undo
+                        </MenubarItem>
+                        <MenubarItem>
+                            Redo
+                        </MenubarItem>
+                        <MenubarItem>Cut</MenubarItem>
+                        <MenubarItem>Copy</MenubarItem>
+                        <MenubarItem>Paste</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+                <MenubarMenu>
+                    <MenubarTrigger>Team</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarRadioGroup>
+                            <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                            <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                            <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                        </MenubarRadioGroup>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Edit</MenubarItem>
+                        <MenubarSeparator />
+                        <MenubarItem inset>Add Team Member</MenubarItem>
+                        <MenubarItem inset>Contact Instructor</MenubarItem>
+                    </MenubarContent>
+                </MenubarMenu>
+            </Menubar>
+
+            <NavigationMenu>
+                <NavigationMenuList>
+
+                    <NavigationMenuItem>
+                        <Tabs>
+                            <TabsList>
+                                <TabsTrigger value='Text Editor'>Text Editor</TabsTrigger>
+                                <TabsTrigger value='Leaderboard'>Leaderboard</TabsTrigger>
+                            </TabsList>
+                        </Tabs>
+                    </NavigationMenuItem>
+
+                </NavigationMenuList>
+            </NavigationMenu>
+
+            <div className="min-w-20">
+                <Select>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Programming Language"/>
+                    </SelectTrigger>
+                    <SelectContent className="min-w-8">
+                        <SelectGroup>
+                            <SelectLabel>Languages</SelectLabel>
+                            <SelectItem value="Python">Python</SelectItem>
+                            <SelectItem value="Java">Java</SelectItem>
+                            <SelectItem value="JavaScript">JavaScript</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+    )
+}
