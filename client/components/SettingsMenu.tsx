9fcef8c803bf06c689ea6d1fcb99ec9af3c8e736
Austin Adams <asadams110102@gmail.com>
2025-02-14 14:51:27 -0600

    Initial design/setup of settings menu with logout functionality.

---
+++ client/components/SettingsMenu.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useTheme } from 'next-themes';
+import { Button } from "./ui/button";
+import { Settings, Sun, Moon, SunMoon } from "lucide-react";
+import Link from 'next/link';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+const SettingsMenu = () => {
+    const { setTheme } = useTheme();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">
+                    <Settings />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuPortal>
+                <DropdownMenuContent>
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger><SunMoon />Theme</DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <DropdownMenuItem onClick={() => setTheme('light')}>
+                                    <Sun className="pr-0.5" />
+                                    Light
+                                </DropdownMenuItem>
+                                <DropdownMenuItem onClick={() => setTheme('dark')}>
+                                    <Moon className="pr-0.5" />
+                                    Dark
+                                </DropdownMenuItem>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                    <DropdownMenuSeparator />
+                    <Button 
+                        className="w-full"
+                        variant={"destructive"}
+                    >
+                        <Link href="/">Logout</Link>
+                    </Button>
+                </DropdownMenuContent>
+            </DropdownMenuPortal>
+        </DropdownMenu>
+    );
+}
+
+export default SettingsMenu;
